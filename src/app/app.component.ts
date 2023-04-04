c6a62df2984577c8f7a9d39d3a0526c743304ab7
Altin Terstena <alt-2020@outlook.com>
2023-04-04 02:04:55 -0400

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CityGo';
+  theme = 'light';
+
+  ngOnInit(): void {
+    // setInterval(() => this.changeTheme(), 5000);
+  }
+
+  changeTheme(): void {
+    if (this.theme === 'light') {
+      this.theme = 'dark';
+    } else {
+      this.theme = 'light';
+    }
+
+    document.documentElement.style.setProperty('--theme', this.theme);
+  }
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ThemePalette} from '@angular/material/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit {
+  color: ThemePalette = 'accent';
+  checked = false;
+  disabled = false;
+
+  ngOnInit(): void {
+  }
+}
