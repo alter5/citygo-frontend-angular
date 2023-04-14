185df1168637257e6e0a68fd7b5603fc367e257a
Altin Terstena <alt-2020@outlook.com>
2023-04-13 21:03:42 -0400

    add: modal component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,11 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 import { MatCardModule } from "@angular/material/card"
 import { MatButtonModule } from "@angular/material/button"
 import { MatSlideToggleModule } from "@angular/material/slide-toggle"
-import { TestThemeComponent } from './components/test-theme/test-theme.component'
+import { TestThemeComponent } from './components/test-theme/test-theme.component';
+import { ModalComponent } from './components/modal/modal.component'
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NavbarComponent, BaseComponent, TestThemeComponent],
+  declarations: [AppComponent, HomeComponent, NavbarComponent, BaseComponent, TestThemeComponent, ModalComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
