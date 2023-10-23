92c1639ef4aaf1ce5a14292d224a3f0a45a3b568
Altin Terstena <alt-2020@outlook.com>
2023-10-23 02:21:47 -0400

    fix: dropdown animation broken after converting to standalone component

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import routeConfig from './app/routes';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent,
   {
     providers: [
+      importProvidersFrom([BrowserAnimationsModule]),
       provideProtractorTestingSupport(),
       provideRouter(routeConfig)
     ]
