9a37d83414886c1f6f293404cf64e0d7b4f71237
Altin Terstena <alt-2020@outlook.com>
2023-11-15 17:24:20 -0500

    add: wrapper component city search bar

---
+++ src/app/core/components/city-search-bar/city-search-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-city-search-bar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './city-search-bar.component.html',
+  styleUrls: ['./city-search-bar.component.scss']
+})
+export class CitySearchBarComponent {
+
+}
