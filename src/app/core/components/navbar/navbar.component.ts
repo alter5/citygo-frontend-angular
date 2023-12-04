2edde24542f516a0f05d5abb00c63677f6cb23fd
Altin Terstena <alt-2020@outlook.com>
2023-12-04 01:01:37 -0500

    save

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,23 +1,32 @@
 import { Component, OnInit, Inject, Renderer2 } from "@angular/core"
+import { NgIf, AsyncPipe } from "@angular/common"
 import { DOCUMENT } from "@angular/common"
-import { RouterModule } from '@angular/router';
+import { RouterModule } from "@angular/router"
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout"
 
-import { MatSlideToggleChange, MatSlideToggleModule } from "@angular/material/slide-toggle"
-import { CitySearchBarComponent } from "../city-search-bar/city-search-bar.component";
+import {
+  MatSlideToggleChange,
+  MatSlideToggleModule
+} from "@angular/material/slide-toggle"
+import { CitySearchBarComponent } from "../city-search-bar/city-search-bar.component"
+import { Observable, map } from "rxjs"
 
 @Component({
-    selector: "app-navbar",
-    templateUrl: "./navbar.component.html",
-    styleUrls: ["./navbar.component.scss"],
-    standalone: true,
-    imports: [RouterModule, CitySearchBarComponent, MatSlideToggleModule]
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"],
+  standalone: true,
+  imports: [RouterModule, CitySearchBarComponent, MatSlideToggleModule, NgIf, AsyncPipe]
 })
 export class NavbarComponent implements OnInit {
   isDarkModeEnabled = false
+  isScreenSmall$: Observable<boolean> | null = null
+  isMenuVisible = false
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
-    private renderer: Renderer2
+    private renderer: Renderer2,
+    private breakpointObserver: BreakpointObserver
   ) {}
 
   ngOnInit(): void {
@@ -25,6 +34,17 @@ export class NavbarComponent implements OnInit {
       localStorage.getItem("isDarkModeEnabled") || "false"
     )
     this.changeTheme()
+
+    this.isScreenSmall$ = this.breakpointObserver
+      .observe([Breakpoints.XSmall, Breakpoints.Small])
+      .pipe(map((result) => {
+        console.log("Result: ", result)
+        return result.matches
+      }))
+  }
+
+  toggleMenu(): void {
+    this.isMenuVisible = !this.isMenuVisible
   }
 
   handleDarkModeToggle({ checked }: MatSlideToggleChange): void {
