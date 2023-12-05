4c8f5774a13cb9c2d6633b71cf59138301c21356
Altin Terstena <alt-2020@outlook.com>
2023-12-05 00:40:10 -0500

    save

--- src/app/features/home/components/city-overview-card/city-overview-card.component.ts
+++ src/app/features/home/components/city-overview-card/city-overview-card.component.ts
@@ -1,14 +1,23 @@
-import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from "@angular/common"
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core"
+import { NgFor, NgOptimizedImage, NgIf } from "@angular/common"
+// import img
 
 @Component({
-  selector: 'app-city-overview-card',
+  selector: "app-city-overview-card",
+  templateUrl: "./city-overview-card.component.html",
+  styleUrls: ["./city-overview-card.component.css"],
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  template: "./city-overview-card.component.html",
-  styleUrls: ['./city-overview-card.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule, NgFor],
 })
-export class CityOverviewCardComponent { }
+export class CityOverviewCardComponent implements OnInit {
+  images: string[] | null = null
+
+  ngOnInit(): void {
+    this.images = [
+      "assets/images/city-card-images/ny-skyscraper.jpg",
+      "assets/images/city-card-images/ny-times-square.jpg",
+      "assets/images/city-card-images/ny-centralpark.jpg"
+    ]
+  }
+}
