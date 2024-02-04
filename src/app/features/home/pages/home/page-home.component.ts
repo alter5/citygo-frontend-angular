962c85e106707695913550bcd286aa7ceac7b39d
Altin Terstena <alt-2020@outlook.com>
2024-02-03 22:01:03 -0500

    save

--- src/app/features/home/pages/home/page-home.component.ts
+++ src/app/features/home/pages/home/page-home.component.ts
@@ -6,13 +6,23 @@ import { TripsService } from "src/app/shared/services/trips.service"
 import { Observable } from "rxjs"
 import { Trip } from "src/app/shared/models/trip.model"
 import { InputAutocompleteComponent } from "src/app/shared/components/input-autocomplete/input-autocomplete.component"
+import { MatButtonModule } from "@angular/material/button"
+import { MatIconModule } from "@angular/material/icon"
 
 @Component({
   selector: "app-home",
   templateUrl: "./page-home.component.html",
   styleUrls: ["./page-home.component.scss"],
   standalone: true,
-  imports: [TestThemeComponent, TripOverviewCardComponent, NgFor, AsyncPipe, InputAutocompleteComponent]
+  imports: [
+    TestThemeComponent,
+    TripOverviewCardComponent,
+    NgFor,
+    AsyncPipe,
+    InputAutocompleteComponent,
+    MatButtonModule,
+    MatIconModule
+  ]
 })
 export class PageHomeComponent implements OnInit {
   trips$: Observable<Trip[]> | undefined
