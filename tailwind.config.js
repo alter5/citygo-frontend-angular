137b8b8a0b7ceab107d338a673110289ef4d22b3
Altin Terstena <alt-2020@outlook.com>
2024-02-03 23:00:03 -0500

    add: create custom palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        'max-container-width': '64rem'
+      }
+    },
   },
   plugins: [],
   blocklist: ['container']
