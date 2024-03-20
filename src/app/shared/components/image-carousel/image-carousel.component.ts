6c8a6765912906c09bb917ed11fa4294e0789a51
Altin Terstena <alt-2020@outlook.com>
2024-03-20 18:10:44 -0400

    style: padding

--- src/app/shared/components/image-carousel/image-carousel.component.ts
+++ src/app/shared/components/image-carousel/image-carousel.component.ts
@@ -11,24 +11,14 @@ import { ImageCarouselSlide } from "./image-carousel-slide.model"
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ImageCarouselComponent {
-  @Input() slides: ImageCarouselSlide[] = [
-    {
-      imageUrl: "https://images.unsplash.com/photo-1485470733090-0aae1788d5af?q=80&w=1217&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      description: "Times Square"
-    },
-    {
-      imageUrl: "https://images.unsplash.com/photo-1485470733090-0aae1788d5af?q=80&w=1217&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      description: "Times Square"
-    },
-    {
-      imageUrl: "https://images.unsplash.com/photo-1485470733090-0aae1788d5af?q=80&w=1217&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      description: "Times Square"
-    }
-  ] // replace these with your actual image paths
+  @Input() slides: ImageCarouselSlide[] | null = null // replace these with your actual image paths
 
   currentSlideIndex = 0
 
   changeImage(forward: boolean) {
+    if (this.slides === null || this.slides.length < 1) {
+      return
+    }
     if (forward) {
       this.currentSlideIndex = (this.currentSlideIndex + 1) % this.slides.length
     } else {
