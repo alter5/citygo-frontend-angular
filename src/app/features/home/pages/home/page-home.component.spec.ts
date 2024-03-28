18e537161948008ab436ece0e3fc977599d7cb6e
Altin Terstena <alt-2020@outlook.com>
2024-03-27 22:27:43 -0400

    add: test case

--- src/app/features/home/pages/home/page-home.component.spec.ts
+++ src/app/features/home/pages/home/page-home.component.spec.ts
@@ -3,7 +3,7 @@ import { RouterTestingModule } from "@angular/router/testing"
 import { HttpClientTestingModule } from "@angular/common/http/testing"
 import { PageHomeComponent } from "./page-home.component"
 import { TripsService } from "src/app/shared/services/trips.service"
-import { of, take } from "rxjs"
+import { delay, first, of, take } from "rxjs"
 import { Trip } from "src/app/shared/models/trip.model"
 import { getMockTrip } from "src/app/shared/models/tripMock"
 
@@ -14,7 +14,8 @@ describe("PageHomeComponent", () => {
 
   beforeEach(async () => {
     tripsServiceSpy = jasmine.createSpyObj("TripsService", ["getPopularTrips"])
-    tripsServiceSpy.getPopularTrips.and.returnValue(of(new Array(3).fill(getMockTrip())))
+
+    // Delay is used to simulate the time it takes for the service to return data
 
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule, HttpClientTestingModule, PageHomeComponent],
@@ -30,31 +31,40 @@ describe("PageHomeComponent", () => {
     component = fixture.componentInstance
   })
 
-  it("should start with 4 mock trips before tripsService returns data", fakeAsync(() => {
-    // continue research: GOOGLE: angular testing with observables ngoninit
-    const compiled = fixture.nativeElement
-
-    fixture.detectChanges()
-
-    component.trips$.pipe(take(1)).subscribe((trips) => {
-      expect(trips.length).toEqual(4)
-
-      const tripCardElements = compiled.querySelectorAll("app-trip-overview-card")
-      expect(tripCardElements.length).toEqual(4)
-    })
-
-    tick()
-  }))
-
   it("should render trips", () => {
-    const mockTrip: Partial<Trip> = { title: "Mock Trip" }
+    const mockTrip = getMockTrip()
+    mockTrip.title = "Test title"
     const mockTrips = new Array(6).fill(mockTrip)
+
+    tripsServiceSpy.getPopularTrips.and.returnValue(of(mockTrips))
 
     fixture.detectChanges()
 
     // check how many app-trip-overview-card components are rendered
     const tripElements = fixture.nativeElement.querySelectorAll("app-trip-overview-card")
 
-    expect(tripElements.length).toEqual(4)
+    expect(tripElements.length).toEqual(6)
+  })
+
+  it("should render dummy trips while loading data from service", async () => {
+    // Delay is used to simulate the time it takes for the service to return data
+    tripsServiceSpy.getPopularTrips.and.returnValue(
+      of(new Array(3).fill(getMockTrip())).pipe(delay(1))
+    )
+
+    fixture.detectChanges()
+
+    const compiled = fixture.nativeElement
+
+    component.trips$.pipe(first()).subscribe((trips) => {
+      // The component should not render the actual trips until the service returns data
+      expect(trips.length).not.toEqual(3)
+
+      // The number of dummy trips created in ngOnInit
+      expect(trips.length).toEqual(4)
+
+      const tripCardElements = compiled.querySelectorAll("app-trip-overview-card")
+      expect(tripCardElements.length).toEqual(4)
+    })
   })
 })
--- src/app/features/home/pages/home/page-home.component.ts
+++ src/app/features/home/pages/home/page-home.component.ts
@@ -3,7 +3,7 @@ import { NgFor, AsyncPipe } from "@angular/common"
 import { TestThemeComponent } from "../../components/test-theme/test-theme.component"
 import { TripOverviewCardComponent } from "../../components/trip-overview-card/trip-overview-card.component"
 import { TripsService } from "src/app/shared/services/trips.service"
-import { Observable, startWith } from "rxjs"
+import { Observable, startWith, tap } from "rxjs"
 import { Trip } from "src/app/shared/models/trip.model"
 import { InputAutocompleteComponent } from "src/app/shared/components/input-autocomplete/input-autocomplete.component"
 import { NgClass } from "@angular/common"
@@ -33,8 +33,11 @@ export class PageHomeComponent implements OnInit {
   constructor(private tripsService: TripsService) {}
 
   ngOnInit(): void {
-    this.trips$ = this.tripsService
-      .getPopularTrips()
-      .pipe(startWith(new Array(4).fill(getMockTrip())))
+    this.trips$ = this.tripsService.getPopularTrips().pipe(
+      startWith(new Array(4).fill(getMockTrip())),
+      tap((trips) => {
+        console.log("🚀 ~ PageHomeComponent ~ ngOnInit ~ trips:", trips.length)
+      })
+    )
   }
 }
